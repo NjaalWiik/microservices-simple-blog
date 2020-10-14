a444448ce03a6d5b69ed0d5437f7845aedff9e7c
Njaal Wiik <njaal.wiik@gmail.com>
2020-10-14 16:09:01 +0200

    Created query server

--- comments/index.js
+++ comments/index.js
@@ -10,11 +10,11 @@ app.use(cors());
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', async (req, res) => {
+app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
@@ -24,8 +24,8 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
-  axios.post('http://localhost:4005/event', {
-    type: 'commentCreated',
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
     data: {
       id: commentId,
       content,
@@ -34,6 +34,12 @@ app.post('/posts/:id/comments', (req, res) => {
   });
 
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Event Received:', req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
